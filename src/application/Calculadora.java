5cd1bee0325b843cc8ed61a2f565c4541d904e9b
Jorge Rodrigues <89171552+jr1522@users.noreply.github.com>
2022-07-07 16:10:08 -0300

    Update Calculadora.java

--- src/application/Calculadora.java
+++ src/application/Calculadora.java
@@ -15,16 +15,16 @@ public class Calculadora {
 		do {
 			System.out.printf("1 - Soma \n"
 					+ "2 - Subtracao \n"
-					+ "3 - Multiplicao \n"
+					+ "3 - MultiplicaÃ§Ã£o \n"
 					+ "4 - Divisao \n"
 					+ "0 - Sair\n\n");
 			opcao = sc.nextInt();
 			
 			if (opcao > 0 && opcao < 5) {
 
-				System.out.println("Digite um número: ");
+				System.out.println("Digite um nÃºmero: ");
 				num1 = sc.nextInt();
-				System.out.println("Digite outro número: ");
+				System.out.println("Digite outro nÃºmero: ");
 				num2 = sc.nextInt();
 
 				switch (opcao) {
@@ -37,24 +37,24 @@ public class Calculadora {
 					System.out.println();
 					break;
 				case 2:
-					System.out.printf("Subtração: \n" + (num1 - num2));
+					System.out.printf("SubtraÃ§Ã£o: \n" + (num1 - num2));
 					System.out.println();
 					break;
 				case 3:
-					System.out.printf("Multiplicação: \n" + (num1 * num2));
+					System.out.printf("MultiplicaÃ§Ã£o: \n" + (num1 * num2));
 					System.out.println();
 					break;
 				case 4:
 					while (num2 == 0) {
-						System.out.println("Divisão por 0 não existe!\nDigite outro valor:\n");
+						System.out.println("DivisÃ£o por 0 nÃ£o existe!\nDigite outro valor:\n");
 						num2 = sc.nextInt();
 						System.out.println();
 					}
-					System.out.printf("Divisão: \n" + (num1 / num2));
+					System.out.printf("DivisÃ£o: \n" + (num1 / num2));
 					System.out.println();
 					break;
 				default:
-					System.out.println("Operação inválida \n ");
+					System.out.println("OperaÃ§Ã£o invÃ¡lida \n ");
 
 				}
 			}
